e5400d8aeb2e81c704ae1e0c69f09a786fb1a71c
yhwa <global user>
2023-02-25 23:08:10 +0900

    useLocation으로 데이터 호출 방식 변경, coin info/price api연동

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,123 @@
-import { Outlet, useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+interface RouterState {
+  state: {
+    state: {
+      name: string
+    }
+  }
+}
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface priceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
 
 function Coin() {
+
+  const Container = styled.div`
+    background-color:${props => props.theme.bgColor};
+    padding:0px 20px;
+    max-width:480px;
+    margin:auto;
+`;
+
+  const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+  const Title = styled.h1`
+    color:${(props) => props.theme.accentColor};
+    font-size:30px;
+`;
+
+  const Loader = styled.div`
+    text-align: center;
+`;
+
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
-  console.log(coinId);
+  const { state: { state } } = useLocation() as RouterState;
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<priceData>();
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+
+    })();
+  }, [coinId]);
+
   return (
-    <div>
-      <h1>Coin : {coinId}</h1>
-      <Outlet />
-    </div>
+    <Container>
+      <Header>
+        <Title>{state.name}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
   );
 }
+
 export default Coin;
