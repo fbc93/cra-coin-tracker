8f54fe039bae077d1db52cbca413ef6ac6802ea1
yhwa <global user>
2023-02-28 14:19:34 +0900

    Revert "Merge pull request #9 from fbc93/feature/0005_api_data_react_query_refactory"
    This reverts commit 20efbf7f4bb67d690971640767646a8268e52735, reversing
    changes made to 7ad5125b4e614f13197b3975e0eb1e5b5c48f722.

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,8 +1,7 @@
-import styled from "styled-components";
 import { Outlet, useLocation, useMatch, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { useQuery } from "react-query";
-import { fetchCoinInfo, fetchCoinTickers } from "../api";
 
 const Container = styled.div`
   background-color:${props => props.theme.bgColor};
@@ -79,29 +78,89 @@ interface RouterState {
   }
 }
 
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface priceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
 
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
   const { state } = useLocation() as RouterState;
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<priceData>();
   const priceMatch = useMatch(`/:coinId/information`);
   const chartMatch = useMatch(`/:coinId/chart`);
 
-  const { isLoading: infoLoading, data: infoData } = useQuery(
-    ["info", coinId],
-    () => fetchCoinInfo(String(coinId))
-  );
+  useEffect(() => {
+    (async () => {
+      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
 
-  const { isLoading: tickersLoading, data: tickersData } = useQuery(
-    ["tickers", coinId],
-    () => fetchCoinTickers(String(coinId))
-  );
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
 
-  const loading = infoLoading || tickersLoading;
+    })();
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name ? state.name : loading ? "Loading..." : infoData?.name}</Title>
+        <Title>{state?.name ? state.name : loading ? "Loading..." : info?.name}</Title>
       </Header>
       {loading ? (<Loader>Loading...</Loader>
       ) : (
@@ -109,28 +168,28 @@ function Coin() {
           <Overview>
             <OverviewItem>
               <span>RANK</span>
-              <span>{infoData?.rank}</span>
+              <span>{info?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>SYMBOL</span>
-              <span>{infoData?.symbol}</span>
+              <span>{info?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>OPEN SOURCE</span>
-              <span>{infoData?.open_source ? "YES" : "NO"}</span>
+              <span>{info?.open_source ? "YES" : "NO"}</span>
             </OverviewItem>
           </Overview>
 
-          <Description>{infoData?.description}</Description>
+          <Description>{info?.description}</Description>
 
           <Overview>
             <OverviewItem>
               <span>TOTAL SUPPLY</span>
-              <span>{tickersData?.total_supply}</span>
+              <span>{priceInfo?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>MAX SUPPLY</span>
-              <span>{tickersData?.max_supply}</span>
+              <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
         </>
